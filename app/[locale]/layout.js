ab672d4532c93b7aae4af7e267194e162e2bd948
Christian Tal <ChrisCoolDev>
2025-06-07 01:01:19 +0300

    preparing for deployment

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -12,57 +12,25 @@ const ibmPlexSans = IBM_Plex_Sans({
  const Jeko = localFont({
   src: [
     {
-      path: '../font/Jeko/jekoDEMO-ExtraBold.otf',
+      path: '../../font/Jeko/jekoDEMO-ExtraBold.otf',
       weight: '900',
       style: 'normal',
     },
     {
-      path: '../font/Jeko/jekoDEMO-ExtraLight.otf',
+      path: '../../font/Jeko/jekoDEMO-ExtraLight.otf',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../font/Jeko/jekoDEMO-Medium.otf',
+      path: '../../font/Jeko/jekoDEMO-Medium.otf',
       weight: '500',
       style: 'normal',
     },
   ]
  })
 
- const Satoshi = localFont({
-  src: [
-    {
-      path: '../font/Satoshi/Satoshi-Black.otf',
-      weight: '900',
-      style: 'normal'
-    },
-    {
-      path: '../font/Satoshi/Satoshi-Bold.otf',
-      weight: '700',
-      style: 'normal'
-    },
-    {
-      path: '../font/Satoshi/Satoshi-Medium.otf',
-      weight: '500',
-      style: 'normal'
-    },
-    {
-      path: '../font/Satoshi/Satoshi-Regular.otf',
-      weight: '400',
-      style: 'normal'
-    },
-    {
-      path: '../font/Satoshi/Satoshi-Light.otf',
-      weight: '300',
-      style: 'normal'
-    },
-  ],
-  variable: '--font-satoshi',
-  display: 'swap',
- })
-
 export const metadata = {
-  title: "ChrisFacile_Dev",
+  title: "Chris",
   description: "Mon portfolio personnel pour vous présenter mes compétences en développement web.",
 };
 
